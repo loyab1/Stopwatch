2481c58c5562e0bd3b0ce40a14e0713f91f303fb
loyab1 <loyab1@montclair.edu>
2025-11-14 16:42:11 -0500

    Fundamental Code Needed for Application Window

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+
+    public static void main (String[] args){
+
+        //Variable for display object calling the constructor of StopWatchDisplay class
+        StopWatchDisplay stopwatchdisplay = new StopWatchDisplay();
+    }
+
 }
--- src/StopWatchDisplay.java
+++ src/StopWatchDisplay.java
@@ -1,2 +1,43 @@
+
+//Necessary Imports to create a Window
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
 public class StopWatchDisplay {
+    /*
+      This Class will Create the Frame needed to display the Window
+      and all the basic variables like its size.
+     */
+
+    //A top level window variable with the name mainWindow;
+    JFrame mainWindow;
+
+    //Display Attributes
+    public void display(){
+
+        //Creates a new Frame method and assigns the frame title
+        mainWindow = new JFrame("Stopwatch");
+        //How big the window is
+        mainWindow.setSize(400,400);
+
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Makes the application Visible
+        mainWindow.setVisible(true);
+
+
+    }
+
+    //Constructor class that calls the display
+    StopWatchDisplay(){
+        display();
+    }
+
+
+
+
+
+
+
 }
